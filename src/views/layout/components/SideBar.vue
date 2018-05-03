4344d797b0c4e64e3dd79e2e9f6eea70697df631
jaak <zhangykvip@126.com>
2018-05-03 20:34:24 +0800

    menus

--- src/views/layout/components/SideBar.vue
+++ src/views/layout/components/SideBar.vue
@@ -9,8 +9,20 @@
       text-color="#bfcbd9"
       active-text-color="#409EFF"
     >
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+          <el-menu-item index="1-3">选项3</el-menu-item>
 
+
+      </el-submenu>
     </el-menu>
+
+
 
 </template>
 
